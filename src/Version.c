24e7e4965b84fff35c7cad7c50cdcfe57e026d9d
Stephen Wasilewski <stephanwaz@gmail.com>
2022-04-25 09:51:52 +0200

    updated radiance source

--- src/Version.c
+++ src/Version.c
@@ -1,1 +1,1 @@
-char VersionID[]="RADIANCE 5.4a (https://github.com/LBNL-ETA/Radiance/tree/9e3e6287) compiled 2022-04-22 for raytraverse";
+char VersionID[]="RADIANCE 5.4a (https://github.com/LBNL-ETA/Radiance/tree/db4988bb) compiled 2022-04-25 for raytraverse";
