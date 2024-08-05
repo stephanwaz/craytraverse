3127333fc1ae90e993e5d70ddf0405d0f8d78e88
Stephen Wasilewski <stephanwaz@gmail.com>
2024-08-05 16:34:34 +0200

    remove from_pdf (not used)

--- src/Version.c
+++ src/Version.c
@@ -1,1 +1,1 @@
-char VersionID[]="RADIANCE 5.4a (https://github.com/LBNL-ETA/Radiance/tree/3d14c7e3) compiled 2024-05-07 for raytraverse";
+char VersionID[]="RADIANCE 5.4a (https://github.com/LBNL-ETA/Radiance/tree/3d14c7e3) compiled 2024-08-05 for raytraverse";
