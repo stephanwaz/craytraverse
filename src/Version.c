5251add7a5b4b4b37611467373cef772b89f6e74
Stephen Wasilewski <stephanwaz@gmail.com>
2024-08-05 16:31:25 +0200

    rollback memory stuff

--- src/Version.c
+++ src/Version.c
@@ -1,1 +1,1 @@
-char VersionID[]="RADIANCE 5.4a (https://github.com/LBNL-ETA/Radiance/tree/3d14c7e3) compiled 2023-06-27 for raytraverse";
+char VersionID[]="RADIANCE 5.4a (https://github.com/LBNL-ETA/Radiance/tree/3d14c7e3) compiled 2024-08-05 for raytraverse";
