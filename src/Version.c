4bdafbbd781d9eedabec155eba5b8e938c056a4a
Stephen Wasilewski <stephanwaz@gmail.com>
2024-11-12 10:28:08 +0100

    switched to pip to install pybind

--- src/Version.c
+++ src/Version.c
@@ -1,1 +1,1 @@
-char VersionID[]="RADIANCE 5.4a (https://github.com/LBNL-ETA/Radiance/tree/3d14c7e3) compiled 2024-10-31 for raytraverse";
+char VersionID[]="RADIANCE 5.4a (https://github.com/LBNL-ETA/Radiance/tree/3d14c7e3) compiled 2024-11-12 for raytraverse";
